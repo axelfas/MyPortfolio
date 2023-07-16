0e1707d169c45c669e597baf1f259dda510bd399
axelfas08 <axelfas08@gmail.com>
2023-07-16 12:48:53 +0200

    formulario y algunas modificaciones

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ const navbarMenu = document.querySelector('.navbar-menu');
 
     var copy = document.querySelector(".logos-slide").cloneNode(true);
     document.querySelector(".logos").appendChild(copy);
+
+
+    function checkInput() {
+        var input = document.getElementById('name');
+        var label = document.getElementById('firslabel');
+      
+        if (input.value !== '') {
+          label.style.display = 'none';
+        } else {
+          label.style.display = 'block';
+        }
+      }
+      
